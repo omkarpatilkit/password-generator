81ea36d2a0b34b3c66e84e69cc3aea1891ba31a6
omkarpatilkit <41175678+omkarpatilkit@users.noreply.github.com>
2021-01-16 12:59:28 +0530

    Create python code.py

---
+++ python code.py
@@ -0,0 +1,33 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+l = []
+n = []
+s = []
+
+for i in range(nr_letters):
+    a = random.randint(0, len(letters)-1)
+    l.append(letters[a])
+print(l)
+for i in range(nr_numbers):
+print(n)
+
+for i in range(nr_symbols):
+print(s)
+
+final = l+s+n
+pswd = ''
+
+set_final = set(final)0
+print(set_final)
+for i in set_final:
+    pswd = pswd.join(i)
+
+print(pswd)
